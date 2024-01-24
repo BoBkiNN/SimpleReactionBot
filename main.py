1bb21c6210cba98ebe44394504993ef1cf2e44b9
BoBkiNN_ <bobkin.yangirov@gmail.com>
2024-01-24 18:01:27 +0600

    multi role selection

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ from disnake.ext import commands
 
 intents = disnake.Intents.default()
 intents.reactions = True
-
 bot = commands.InteractionBot(intents=intents)
 
-MESSAGE_ID = 211111111111111111111
-ROLE_ID = 1111111111111111111111
-TOKEN = "None"
-EMOJI_ID = None # set to none to trigger on any emoji
+########### CONFIG ###########
+
+MESSAGE_ID = 0
+TOKEN = "TOKEN"
+EMOJI_TO_ROLE = {
+    
+}
+
+## Example EMOJI_TO_ROLE:
+# EMOJI_TO_ROLE = {
+#    "💀": 1199390475912761424
+# }
+
+######### CONFIG END #########
 
 @bot.event
 async def on_ready():
@@ -17,36 +26,34 @@ async def on_ready():
 
 @bot.event
 async def on_raw_reaction_add(payload: disnake.RawReactionActionEvent):
-    if EMOJI_ID != None and EMOJI_ID != payload.emoji.id:
+    rid = EMOJI_TO_ROLE.get(str(payload.emoji))
+    if rid == None or payload.message_id != MESSAGE_ID:
         return
     channel = bot.get_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
-    
-    if payload.message_id == MESSAGE_ID:
-        role = message.guild.get_role(ROLE_ID)
-        if role == None:
-            return
-        member = await message.guild.get_or_fetch_member(payload.user_id)
-        await member.add_roles(role, reason=f"Reacted on {message.id}")
-        print("Given role to", member.id)
+    role = message.guild.get_role(rid)
+    if role == None:
+        return
+    member = await message.guild.get_or_fetch_member(payload.user_id)
+    await member.add_roles(role, reason=f"Reacted on {message.id}")
+    print(f"Given role {rid} to", member.id)
 
 @bot.event
 async def on_raw_reaction_remove(payload: disnake.RawReactionActionEvent):
-    if EMOJI_ID != None and EMOJI_ID != payload.emoji.id:
+    rid = EMOJI_TO_ROLE.get(str(payload.emoji))
+    if rid == None or payload.message_id != MESSAGE_ID:
         return
     channel = bot.get_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
-    
-    if payload.message_id == MESSAGE_ID:
-        role = message.guild.get_role(ROLE_ID)
-        if role == None:
-            return
-        member = await message.guild.get_or_fetch_member(payload.user_id)
-        await member.remove_roles(role, reason=f"Unreact {message.id}")
-        print("Revoked role from", member.id)
+    role = message.guild.get_role(rid)
+    if role == None:
+        return
+    member = await message.guild.get_or_fetch_member(payload.user_id)
+    await member.remove_roles(role, reason=f"Unreact {message.id}")
+    print(f"Revoked role {rid} from", member.id)
 
 if __name__ == "__main__":
-    if MESSAGE_ID == None or ROLE_ID == None or TOKEN == None or TOKEN == "TOKEN":
-        print("Message id or role id or token not set")
+    if MESSAGE_ID == None or MESSAGE_ID == 0 or len(EMOJI_TO_ROLE) == 0 or TOKEN == None or TOKEN == "TOKEN":
+        print("Message id or EMOJI_TO_ROLE map or token not set")
         exit(0)
     bot.run(TOKEN)
